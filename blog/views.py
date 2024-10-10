5c69254cbbae43f0c2a42d15400d4fc3f9ec01d6
Brindha <brindha@nidrive.jp>
2024-10-10 20:29:19 +0530

    third commit

--- blog/views.py
+++ blog/views.py
@@ -2,26 +2,33 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.urls import reverse
 import logging
+from .models import Post
 
 # Create your views here.
-
-post = [
-        {'id': 1, 'title':'post 1', 'content':'content of post 1'},
-        {'id': 2,'title':'post 2', 'content':'content of post 2'},
-        {'id': 3,'title':'post 3', 'content':'content of post 3'},
-        {'id': 4,'title':'post 4', 'content':'content of post 4'},
-    ]
+#static demo data
+#post = [
+ #       {'id': 1, 'title':'post 1', 'content':'content of post 1'},
+  #      {'id': 2,'title':'post 2', 'content':'content of post 2'},
+    #    {'id': 3,'title':'post 3', 'content':'content of post 3'},
+   #     {'id': 4,'title':'post 4', 'content':'content of post 4'},
+    #
 def index(request):
     blog_title="Brindha Latest Posts"
+    #getting data from the post model
+    posts = Post.objects.all()
     #return HttpResponse("Hello world , you are the blog index")
-    return render(request,'blog/index.html',{'blog_title':blog_title,'post':post})
+    return render(request,'blog/index.html',{'blog_title':blog_title,'post':posts})
 
 def detail(request,post_id):
     #return HttpResponse(f"you are viewing the post details page and the ID is {post_id}")
-    posts=next((item for item in post if item['id']== int(post_id)),None)
+    #getting data from model by post modelid
+    post = Post.objects.get(pk=post_id)
+
+    #posts=next((item for item in posts if item['id']== int(post_id)),None)
+
     #logger=logging.getLogger("testing")
     #logger.debug(f'post variable is {posts}')
-    return render(request,'blog/detail.html',{'posts':posts})
+    return render(request,'blog/detail.html',{'posts':post})
 
 def old_url_redirect(request):
     return redirect(reverse('blog:new_page_url'))
